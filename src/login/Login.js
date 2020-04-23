f53e4ea84a4f8c04a21cca574228d6fe9823f8d7
Felype Santiago Ferreira <felype.ferreira@gmail.com>
2020-04-23 13:09:51 +0300

    Initial commit.

---
+++ src/login/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+      <div className="wrapper">
+        <div className='container'>
+          <div className="card">
+          <div className="card-header">The Bank</div>
+          <div className="card-body">
+              <h5 class="card-title">Login</h5>
+              
+          </div>
+          <div className="card-footer text-muted">
+              <a href="#">Forgot password?</a>
+          </div>
+      
+          </div>
+
+        </div>
+      </div>
+      
+      
+    );
+  }
+  
+  export default Login;
