4ae22cad7980ad78861b713c6f057e78436830a3
Felype Santiago Ferreira <felype.ferreira@gmail.com>
2020-04-23 17:38:43 +0300

    Added login form.

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css';
+import LoginForm from './LoginForm';
 
 function Login() {
     return (
@@ -9,7 +10,7 @@ function Login() {
           <div className="card-header">The Bank</div>
           <div className="card-body">
               <h5 class="card-title">Login</h5>
-              
+              <LoginForm />
           </div>
           <div className="card-footer text-muted">
               <a href="#">Forgot password?</a>
--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function LoginForm() {
-    return <div></div>
+    return (
+        <Formik 
+        initialValues={{username: '', password: ''}} 
+        validationSchema={Yup.object({
+            username: Yup.string()
+                .required('User name is required to log in your bank account.'),
+            password: Yup.string()
+                .required('Password is required to login in your bank account.')
+        })}>
+            <Form>
+                <div className="form-group">
+                    <label htmlFor="username">User name</label>
+                    <Field className="form-control" name="username" type="text" />
+                    <ErrorMessage name="username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <Field className="form-control" name="password" type="password" />
+                    <ErrorMessage name="password" />
+                </div>
+                <button type="submit" className="btn btn-primary" style={{marginTop: '25px', float: 'right', margin: '20px 0', width: '150px'}}>Login</button>
+            </Form>
+          </Formik>
+    );
 }
 
 export default LoginForm;
